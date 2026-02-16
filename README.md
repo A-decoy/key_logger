788ff8d42506f7f979f804a69dac795189e4202a
A-decoy <sarasokheynasaroeun@gmail.com>
2026-02-17 01:28:48 +1000

    added word counter

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # key_logger
 idk what to do with this
 
-it captures the keys you click and click ctrl+space to end. The program will print the most used keys
+it captures the keys you click and click ctrl+space to end. The program will print the most used keys and words
+very buggy
+
+may or may not fixed
--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import keyboard
 
 key_dict = {}
+word_dict = {}
+word = ""
+buffer = ""
+command_lst = ["space", "shift", "ctrl", "alt", "backspace"]
+
+def counter(key, dict_type):
+    if key in dict_type.keys():
+        dict_type[key] +=1
+    else:
+        dict_type[key] = 1
+    
+def sort_things(dict_type):
+    sorted_tuples = sorted(dict_type.items(), key=lambda num_key: num_key[1], reverse=True)
+    return dict(sorted_tuples)
+
+def print_things(dict_type):
+    for key, value in dict_type.items():
+        print(f"{key:7}:{value:7}") # will fix later
+                
 
 def on_key(event):
+    global word
+    global buffer
     if event.event_type == "down":
-        if event.name in key_dict.keys():
-            key_dict[f"{event.name}"]+= 1
-        else:
-            key_dict[f"{event.name}"] = 1
-
+        if event.name not in command_lst:
+            word += event.name
+            counter(event.name, key_dict)
+        elif event.name == command_lst[0]:
+            counter(word, word_dict)
+            word = ""
+        
+ 
 keyboard.hook(on_key)
 
-keyboard.wait(hotkey='ctrl+space')
+keyboard.wait(hotkey='ctrl+alt')
 
-sorted_tuples = sorted(key_dict.items(), key=lambda num_key: num_key[1], reverse=True)
-
-sorted_key = dict(sorted_tuples)
-
-for key, value in sorted_key.items():
-    print(f"{key:7}:{value:7}")
+print_things(sort_things(key_dict))
+print("----------------------------------------------------") # will fix later
+print_things(sort_things(word_dict))
